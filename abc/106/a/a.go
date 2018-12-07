8432a15162d1e5433cc2b6d09dc939c92ff5526c
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-12-08 00:39:27 +0900

    Add abc106

---
+++ abc/106/a/a.go
@@ -0,0 +1,17 @@
+// Package main provides
+//
+// File:  a.go
+// Author: ymiyamoto
+//
+// Created on Thu Dec  6 23:48:41 2018
+//
+package main
+
+import "fmt"
+
+func main() {
+	var A, B int
+	fmt.Scan(&A, &B)
+
+	fmt.Println((A - 1) * (B - 1))
+}
