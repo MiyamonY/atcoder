926ca7578d1529a3380fcb379c1dbcd4cc2e4ef6
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-02-18 01:22:36 +0900

    Add abc072

---
+++ abc/072/c/02/src/main.rs
@@ -0,0 +1,66 @@
+macro_rules! scan {
+    () => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().to_string()
+        }
+    };
+    (;;$n:expr) => {
+        (0..$n).map(|_| scan!()).collect::<Vec<_>>()
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty;;$n:expr) => {
+        (0..$n).map(|_| scan!($t;;)).collect::<Vec<_>>()
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let _ = scan!(usize);
+    let v = scan!(usize;;);
+
+    let mut num = vec![0; 100_000 + 1];
+    for &n in v.iter() {
+        if n > 0 {
+            num[n - 1] += 1;
+        }
+        num[n] += 1;
+        num[n + 1] += 1;
+    }
+    println!("{}", num.iter().max().unwrap())
+}
