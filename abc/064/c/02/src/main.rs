798ee34f370b030266626581c6250ff00a59b5b7
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-02-23 18:30:46 +0900

    Add abc 064

---
+++ abc/064/c/02/src/main.rs
@@ -0,0 +1,78 @@
+use std::collections::HashMap;
+
+#[allow(unused_macros)]
+macro_rules! scan {
+    () => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().to_string()
+        }
+    };
+    (;;$n:expr) => {
+        (0..$n).map(|_| scan!()).collect::<Vec<_>>()
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty;;$n:expr) => {
+        (0..$n).map(|_| scan!($t;;)).collect::<Vec<_>>()
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let n = scan!(usize);
+    let vs = scan!(i64;;);
+
+    let mut m = HashMap::new();
+    for &v in vs.iter() {
+        let n = if v >= 3200 { 8 } else { v / 400 };
+        let val = m.entry(n).or_insert(0);
+        *val += 1
+    }
+
+    let s = m.len();
+    if m.contains_key(&8) {
+        let &w = m.get(&8).unwrap();
+        if n == w {
+            println!("1 {}", w)
+        } else {
+            println!("{} {}", s - 1, s + w - 1)
+        }
+    } else {
+        println!("{} {}", s, s)
+    }
+}
