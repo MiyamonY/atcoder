83c897c7d9b7206c41ef87158da226d8dbd36ca0
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-03-03 17:13:08 +0900

    Add abc053

---
+++ abc/053/d/01/src/main.rs
@@ -0,0 +1,91 @@
+use std::collections::HashMap;
+
+#[allow(unused_macros)]
+macro_rules! scan {
+    () => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().to_string()
+        }
+    };
+    (;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().split_whitespace().map(|s| s.to_string()).collect::<Vec<String>>()
+        }
+    };
+    (;;$n:expr) => {
+        {
+            (0..$n).map(|_| scan!()).collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty;;$n:expr) => {
+        (0..$n).map(|_| scan!($t;;)).collect::<Vec<_>>()
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let _ = scan!(usize);
+    let vs = scan!(i64;;);
+    let mut m = HashMap::new();
+    for &v in vs.iter() {
+        let n = m.entry(v).or_insert(0);
+        *n += 1;
+        if *n == 3 {
+            *n = 1
+        }
+    }
+
+    let (mut num1, mut num2) = (0, 0);
+    for (_, &v) in m.iter() {
+        if v == 1 {
+            num1 += 1
+        } else {
+            num2 += 1
+        }
+    }
+
+    if num2 % 2 == 0 {
+        println!("{}", num1 + num2)
+    } else {
+        println!("{}", num1 - 1 + num2)
+    }
+}
