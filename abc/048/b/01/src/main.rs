fa68d855b50a901f155388c8c8746601aea4b8ff
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-03-08 01:16:37 +0900

    Add abc048

---
+++ abc/048/b/01/src/main.rs
@@ -0,0 +1,72 @@
+#[allow(unused_macros)]
+macro_rules! scan {
+    () => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().to_string()
+        }
+    };
+    (;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().split_whitespace().map(|s| s.to_string()).collect::<Vec<String>>()
+        }
+    };
+    (;;$n:expr) => {
+        {
+            (0..$n).map(|_| scan!()).collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty;;$n:expr) => {
+        (0..$n).map(|_| scan!($t;;)).collect::<Vec<_>>()
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let (a, b, x) = scan!(i64, i64, i64);
+
+    let mut n = b / x + 1;
+    if a > 0 {
+        n -= (a - 1) / x + 1
+    }
+
+    println!("{}", n);
+}
