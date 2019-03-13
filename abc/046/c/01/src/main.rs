31d69f558bad035c5e1f6b06590d754642937337
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-03-14 00:38:52 +0900

    Add abc046

---
+++ abc/046/c/01/src/main.rs
@@ -0,0 +1,88 @@
+use std::cmp::max;
+#[allow(unused_macros)]
+macro_rules! scan {
+    () => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().to_string()
+        }
+    };
+    (;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().split_whitespace().map(|s| s.to_string()).collect::<Vec<String>>()
+        }
+    };
+    (;;$n:expr) => {
+        {
+            (0..$n).map(|_| scan!()).collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty;;$n:expr) => {
+        (0..$n).map(|_| scan!($t;;)).collect::<Vec<_>>()
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let n = scan!(i64);
+    let (mut x, mut y) = (1, 1);
+    for _ in 0..n {
+        let (a, b) = scan!(i64, i64);
+        if x <= a && y <= b {
+            x = a;
+            y = b;
+        } else if x > a && y > b {
+            let n = max((x + a - 1) / a, (y + b - 1) / b);
+            x = n * a;
+            y = n * b;
+        } else if y > b {
+            let n = (y + b - 1) / b;
+            x = n * a;
+            y = n * b;
+        } else {
+            let n = (x + a - 1) / a;
+            x = n * a;
+            y = n * b;
+        }
+    }
+
+    println!("{}", x + y)
+}
