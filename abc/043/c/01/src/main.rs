cd75b02413cc5e963c50a963de83f09f29ad1d32
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-03-18 03:33:59 +0900

    Add abc043

---
+++ abc/043/c/01/src/main.rs
@@ -0,0 +1,84 @@
+use std::cmp::min;
+
+#[allow(unused_macros)]
+macro_rules! scan {
+    () => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().to_string()
+        }
+    };
+    (;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().split_whitespace().map(|s| s.to_string()).collect::<Vec<String>>()
+        }
+    };
+    (;;$n:expr) => {
+        {
+            (0..$n).map(|_| scan!()).collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty;;$n:expr) => {
+        (0..$n).map(|_| scan!($t;;)).collect::<Vec<_>>()
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let n = scan!(i64);
+    let vs = scan!(i64;;);
+    let s: i64 = vs.iter().sum();
+
+    let ave = s / n;
+    let mut total0 = 0;
+    for v in vs.iter() {
+        let d = v - ave;
+        total0 += d * d;
+    }
+
+    let ave = (s + n - 1) / n;
+    let mut total = 0;
+    for v in vs {
+        let d = v - ave;
+        total += d * d;
+    }
+    println!("{}", min(total0, total))
+}
---
+++ abc/043/d/01/src/main.rs
@@ -0,0 +1,82 @@
+use std::collections::HashMap;
+#[allow(unused_macros)]
+macro_rules! scan {
+    () => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().to_string()
+        }
+    };
+    (;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().split_whitespace().map(|s| s.to_string()).collect::<Vec<String>>()
+        }
+    };
+    (;;$n:expr) => {
+        {
+            (0..$n).map(|_| scan!()).collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty;;$n:expr) => {
+        (0..$n).map(|_| scan!($t;;)).collect::<Vec<_>>()
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let s = scan!();
+    let cs: Vec<_> = s.chars().collect();
+    for (i, (c0, c1)) in cs.iter().zip(cs[1..].iter()).enumerate() {
+        if c0 == c1 {
+            println!("{} {}", i + 1, i + 2);
+            return;
+        }
+    }
+
+    for (i, (c0, c2)) in cs.iter().zip(cs[2..].iter()).enumerate() {
+        if c0 == c2 {
+            println!("{} {}", i + 1, i + 3);
+            return;
+        }
+    }
+
+    println!("-1 -1");
+}
