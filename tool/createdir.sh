a9f0d845d6f576879f13d60cd12f38b1a325da5c
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-04-13 11:40:02 +0900

    update tool

--- tool/createdir.sh
+++ tool/createdir.sh
@@ -2,14 +2,14 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-if [ $# -ne 2 ];then
+if [ $# -ne 3 ];then
     echo "createdir.sh needs arguments."
-    echo "createdir.sh ContestName ContestNo"
+    echo "createdir.sh ContestName ContestNo No"
     exit 1
 fi
 
 for problem in a b c d
 do
     mkdir -p "${SCRIPT_DIR}/../$1/$2/${problem}/"
-    cargo new "${SCRIPT_DIR}/../$1/$2/${problem}/01" --name ${problem} --vcs none
+    cargo new "${SCRIPT_DIR}/../$1/$2/${problem}/$3" --name ${problem} --vcs none
 done
