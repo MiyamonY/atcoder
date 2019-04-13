5d857525c37bc5758a0b4a12bea64581c6930dfb
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-04-13 12:59:44 +0900

    Add abc058

---
+++ abc/058/d/02/src/main.rs
@@ -0,0 +1,83 @@
+#[allow(unused_macros)]
+macro_rules! scan {
+    () => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().to_string()
+        }
+    };
+    (;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().split_whitespace().map(|s| s.to_string()).collect::<Vec<String>>()
+        }
+    };
+    (;;$n:expr) => {
+        {
+            (0..$n).map(|_| scan!()).collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty;;$n:expr) => {
+        (0..$n).map(|_| scan!($t;;)).collect::<Vec<_>>()
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+const MOD: i64 = 1_000_000_007;
+
+fn main() {
+    let (_, _) = scan!(usize, usize);
+    let xs = scan!(i64;;);
+    let ys = scan!(i64;;);
+
+    let mut sum_x = 0;
+    for (i, &x) in xs.iter().enumerate() {
+        sum_x += x * i as i64 - (xs.len() - 1 - i) as i64 * x;
+        sum_x %= MOD;
+    }
+
+    let mut sum_y = 0;
+    for (i, &y) in ys.iter().enumerate() {
+        sum_y += y * i as i64 - (ys.len() - 1 - i) as i64 * y;
+        sum_y %= MOD;
+    }
+
+    println!("{}", sum_x * sum_y % MOD);
+}
