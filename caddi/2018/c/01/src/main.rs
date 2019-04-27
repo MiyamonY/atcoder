878190eaaf847ff0d75dc08e3bd35bcd3c2aa695
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-04-28 02:01:32 +0900

    Add caddi2018(c,d)

---
+++ caddi/2018/c/01/src/main.rs
@@ -0,0 +1,86 @@
+use std::collections::HashMap;
+
+#[allow(unused_macros)]
+macro_rules! scan {
+    () => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().to_string()
+        }
+    };
+    (;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().split_whitespace().map(|s| s.to_string()).collect::<Vec<String>>()
+        }
+    };
+    (;;$n:expr) => {
+        {
+            (0..$n).map(|_| scan!()).collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty;;$n:expr) => {
+        (0..$n).map(|_| scan!($t;;)).collect::<Vec<_>>()
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let (n, mut p) = scan!(usize, usize);
+    let mut m = HashMap::new();
+    for i in 2.. {
+        while p % i == 0 {
+            *m.entry(i).or_insert(0) += 1;
+            p /= i;
+        }
+
+        if p <= i * i {
+            *m.entry(p).or_insert(0) += 1;
+            break;
+        }
+    }
+    let mut ans = 1;
+    for (k, v) in m {
+        if n <= v {
+            ans *= k.pow((v / n) as u32);
+        }
+    }
+    println!("{}", ans);
+}
