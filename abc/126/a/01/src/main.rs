4d05f3b5bfbc502e07a0ec89c2e622dcb7abbda6
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-05-21 08:50:35 +0900

    Add abc126

---
+++ abc/126/a/01/src/main.rs
@@ -0,0 +1,74 @@
+#[allow(unused_macros)]
+macro_rules! scan {
+    () => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().to_string()
+        }
+    };
+    (;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().split_whitespace().map(|s| s.to_string()).collect::<Vec<String>>()
+        }
+    };
+    (;;$n:expr) => {
+        {
+            (0..$n).map(|_| scan!()).collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty;;$n:expr) => {
+        (0..$n).map(|_| scan!($t;;)).collect::<Vec<_>>()
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let (n, k) = scan!(usize, usize);
+    let s = scan!(String).chars().collect::<Vec<_>>();
+    for (i, &c) in s.iter().enumerate() {
+        if i + 1 == k {
+            print!("{}", c.to_lowercase().collect::<Vec<_>>()[0]);
+        } else {
+            print!("{}", c);
+        }
+    }
+    println!();
+}
