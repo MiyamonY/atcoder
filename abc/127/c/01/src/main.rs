a38443214633bee225bfe697a817cce753b22ded
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-05-28 06:58:27 +0900

    [WIP] Add abc127(a,b,c,d,e)

---
+++ abc/127/c/01/src/main.rs
@@ -0,0 +1,82 @@
+#[allow(unused_macros)]
+macro_rules! scan {
+    () => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().to_string()
+        }
+    };
+    (;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().split_whitespace().map(|s| s.to_string()).collect::<Vec<String>>()
+        }
+    };
+    (;;$n:expr) => {
+        {
+            (0..$n).map(|_| scan!()).collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty;;$n:expr) => {
+        (0..$n).map(|_| scan!($t;;)).collect::<Vec<_>>()
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let (n, m) = scan!(usize, usize);
+
+    let (mut x, mut y) = (0, n);
+    for _ in 0..m {
+        let (l, r) = scan!(usize, usize);
+        if x < l {
+            x = l
+        }
+        if r < y {
+            y = r;
+        }
+    }
+
+    if y >= x {
+        println!("{}", y - x + 1);
+    } else {
+        println!("0")
+    }
+}
