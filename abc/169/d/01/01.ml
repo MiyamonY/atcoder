bbf4d44415057ebaab24133b95cc3c0b2cf35d35
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2020-06-05 21:23:37 +0900

    Add abc169(a,b,c,d,e)

---
+++ abc/169/d/01/01.ml
@@ -0,0 +1,105 @@
+open Batteries
+module EnumL = Enum.Labels
+module ListL = List.Labels
+module ArrayL = Array.Labels
+
+let dbg0 x = Printf.eprintf "[debug]%s\n" @@ dump x
+let dbg1 x = dbg0 x; x
+
+let id = identity
+
+let (++) n m = List.range n `To m
+let (++-) n m = if n >= m then List.range n `Downto m else []
+let (++^) n m = if n < m then List.range n `To (pred m) else []
+
+let scan fmt = Scanf.sscanf (read_line ()) fmt
+
+let scan_lines n fmt f =
+  if n = 0 then []
+  else List.map (fun _ -> scan fmt f) (1 ++ n)
+
+let scan_list ?sep cnv =
+  let line = read_line () in
+  (match sep with
+   | None -> List.map String.of_char @@ String.to_list line
+   | Some sep -> String.split_on_char sep line)
+  |> List.map cnv
+
+let scan_array ?sep cnv = Array.of_list @@ scan_list ?sep cnv
+
+let scan_matrix n m e ?sep conv =
+  let arr = Array.make_matrix n m e in
+  ListL.iter (0 ++^ n)
+    ~f:(fun i -> arr.(i) <- Array.of_list @@ scan_list ?sep conv);
+  arr
+
+let atoi c = Char.code c - Char.code '0'
+
+let rec powerset e =
+  match Enum.get e with
+  | None -> Enum.singleton @@ Enum.empty ()
+  | Some v ->
+    let f = powerset e in
+    let g = Enum.clone f in
+    EnumL.map f ~f:(fun x -> let y = Enum.clone x in push y v; y)
+    |>  Enum.append g
+
+let permutations l =
+  let rec aux l =
+    let rec interleave x = function
+      | [] -> [[x]]
+      | (hd::tl) as lst ->
+        (x::lst) ::
+        (ListL.map ~f:(List.cons hd)  @@ interleave x tl)
+    in
+    match l with
+    | [] -> [[]]
+    | hd::tl -> List.concat @@ List.map (interleave hd) @@ aux tl in
+  let l = List.sort (List.compare Int.compare) @@ aux @@ List.of_enum l in
+  List.enum % ListL.map ~f:List.enum @@ l
+
+let intersection l =
+  EnumL.filter ~f:(fun x -> EnumL.exists ~f:((=) x) @@ Enum.clone l)
+
+let zip l m =
+  let n = min (List.length l) (List.length m) in
+  List.combine (List.take n l) (List.take n m)
+
+let lower_bound n f =
+  let rec aux l u =
+    if u - l > 1 then
+      let m = (l + u) / 2 in
+      if f m then aux l m
+      else aux m u
+    else u in aux (-1) n
+
+let between n x m = n <= x && x < m
+
+let n = scan "%d" id
+
+let rec divs n i =
+  if n = 1 then []
+  else if n mod i = 0 then i :: divs (n/i) i
+  else if n < i*i then [n]
+  else divs n (succ i)
+
+let rec shrink acc = function
+  | [] -> acc
+  | a::rest ->
+    begin
+      match acc with
+      | (b,n)::res when b = a -> shrink ((b,succ n)::res) rest
+      | _ -> shrink ((a,1)::acc) rest
+    end
+
+exception Found of int
+
+let () =
+  shrink [] @@ divs n 2
+  |> ListL.map ~f:(fun (_,n)->
+      try ListL.iter (0 ++^ 50) ~f:(fun i ->
+          if i * (i+1)/2 <= n && n < (i+1)*(i+2)/2 then raise (Found i)); 0
+      with
+      | Found i ->  i)
+  |> List.sum
+  |> Printf.printf "%d\n"
