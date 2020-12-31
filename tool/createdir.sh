de356dc46821f15765ea527586c19f8ea177c904
MIYAMOTO Yohei <miyamoto@heartorg.co.jp>
2020-12-31 21:17:22 +0900

    Fix tool

--- tool/createdir.sh
+++ tool/createdir.sh
@@ -11,5 +11,5 @@ fi
 for problem in a b c d e f
 do
     mkdir -p "${SCRIPT_DIR}/../$1/$2/${problem}/$3"
-    (cd "${SCRIPT_DIR}/../$1/$2/${problem}/$3"; dune init executable "$3" --libs batteries && dune build)
+    (cd "${SCRIPT_DIR}/../$1/$2/${problem}/$3"; dune init executable "$3" --libs str && dune build)
 done
