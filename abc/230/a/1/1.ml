3a200eb68bb227154470c73b69c7bf35871b4fb4
MIYAMOTO Yohei <miyamoto@heartorg.co.jp>
2021-12-15 17:19:17 +0900

    [Add] abc230(a,b,c,d,e)

---
+++ abc/230/a/1/1.ml
@@ -0,0 +1,118 @@
+module Array = struct
+  include ArrayLabels
+  let min arr = Array.fold_left (fun a v -> min a v) Int.max_int arr
+  let sum arr = Array.fold_left (+) 0 arr
+end
+
+module List = struct
+  include ListLabels
+
+  module Op = struct
+    let (++) n m =
+      let rec aux i acc =
+        if i = m then List.rev @@ m::acc
+        else aux (i+1) @@ i::acc in
+      if n > m then [] else aux n []
+
+    let (++^) n m = n ++ (m-1)
+    let (--) n m = List.rev (m ++ n)
+    let (--^) n m = ((m+1) -- n)
+  end
+
+  open Op
+
+  let init n ~f = ListLabels.map ~f (0++^n)
+
+  let rec drop n = function
+    | [] -> []
+    | ls when n = 0 -> ls
+    | _::ls -> drop (pred n) ls
+
+  let rec take n = function
+    | [] -> []
+    | x::ls ->
+       if n = 0 then []
+       else x :: take (pred n) ls
+
+  let findi ~f ls =
+    let rec aux i =  function
+      | [] -> i
+      | x::ls -> if f x then i else aux (i+1) ls
+    in
+    aux 0 ls
+
+  let rec powerset = function
+    | [] -> [[]]
+    | hd::tl ->
+       let pws =  powerset tl in
+       pws @ ListLabels.map pws ~f:(fun pw -> hd::pw)
+
+  let permutations l =
+    let rec interleave x = function
+      | [] -> [[x]]
+      | (hd::tl) as l ->
+         (x::l) :: (interleave x tl |> ListLabels.map ~f:(fun l -> hd::l)) in
+    let rec aux = function
+      | [] -> [[]]
+      | a::rest ->
+         aux rest |> ListLabels.map ~f:(interleave a) |> List.concat in
+    aux l
+
+  let rec zip a b =
+    match a, b with
+    | [], _ -> []
+    | _, [] -> []
+    | x::a, y::b -> (x,y) :: zip a b
+
+  let sum  = ListLabels.fold_left ~f:(+) ~init:0
+  let min ~cmp ls = List.hd @@ ListLabels.stable_sort ~cmp ls
+  let max ~cmp ls = List.hd @@ ListLabels.stable_sort ~cmp:(fun a b -> - (cmp a b)) ls
+  let of_string s = ListLabels.map ~f:(String.get s) (0 ++^ String.length s)
+end
+
+open List.Op
+
+module Scan = struct
+  let scan fmt f = Scanf.sscanf (read_line ()) fmt f
+
+  let lines n fmt f =
+    List.map ~f:(fun _ -> scan fmt f) (0++^n)
+
+  let list sep f =
+    let line = read_line () in
+    Str.split (Str.regexp_string sep) line
+    |> List.map ~f
+
+  let matrix n m e f =
+    let arr = Array.make_matrix ~dimx:n ~dimy:m e in
+    Array.iteri ~f:(fun i line ->
+        let s = Scanf.Scanning.from_string @@ read_line () in
+        Array.iteri ~f:(fun j _ ->
+            arr.(i).(j) <- Scanf.bscanf s " %s" f;
+          ) line) arr; arr
+end
+
+module Util = struct
+  let debug = ref false
+
+  let max_num = 100_000_000_000
+  let min_num = -100_000_000_000
+  let id = fun x -> x
+  let tuple2 x y = (x,y)
+  let tuple3 x y z = (x,y,z)
+  let tuple4 x y z w = (x,y,z,w)
+  let succ x = x + 1
+  let pred x = x - 1
+  let diff a b = if a > b then a - b else b - a
+  let neg a = - a
+  let between n x m = n <= x && x < m
+  let sqrt_int n = int_of_float @@ sqrt @@ float_of_int n
+  let dbg msg = if !debug then Printf.printf "[debug]%s\n" msg else ()
+end
+
+open Util
+
+let () =
+  let n = Scan.scan "%d" id in
+  if n >= 42 then Printf.printf "AGC%03d\n" @@ succ n
+  else Printf.printf "AGC%03d\n" n
